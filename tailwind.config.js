590070fa20032b67da007c146e21dde886d3f0a5
Luca Dalmasso <lucadalmasso@gmail.com>
2020-11-06 20:02:06 +0100

    started to work on navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   },
   purge: [],
   theme: {
+    colors: {
+      'milk': '#fbf7f0',
+      'purple': '#52057b',
+      'gray': '#f4f4f2'
+    },
     extend: {},
   },
   variants: {},
