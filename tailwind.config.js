88718deed1d1afdf07f217112884d6d041e6a947
Luca Dalmasso <lucadalmasso@gmail.com>
2020-11-07 13:09:12 +0100

    worked on logo - menu - pic - hero section with hover effects

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,7 @@ module.exports = {
   },
   purge: [],
   theme: {
-    colors: {
-      'milk': '#fbf7f0',
-      'purple': '#52057b',
-      'gray': '#f4f4f2'
-    },
+    colors: {},
     extend: {},
   },
   variants: {},
