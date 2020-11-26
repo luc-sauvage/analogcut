3d77e3272cd5c44e3a85ef950595cf0ac3902b6b
Luca Dalmasso <lucadalmasso@gmail.com>
2020-11-26 18:42:52 +0100

    added an animation to navbar on product pages

--- public/script.js
+++ public/script.js
@@ -17,10 +17,12 @@ function growShrinkLogo() {
     if (screen.width > 1100 && document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         console.log("shrinking!");
         navbar.style.height = "6rem";
+        navbar.style.backgroundColor = "black";
         logo.style.width = '12rem';
         logo.style.top = "0";
     } else {
         navbar.style.height = "8rem";
+        navbar.style.background = "none";
         if (screen.width > 1100) {
             logo.style.top = "30px"; 
             logo.style.width = '23rem';
