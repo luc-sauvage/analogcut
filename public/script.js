9a680d8282a263a8962da2182e06cb3d7930d9cb
Luca Dalmasso <lucadalmasso@gmail.com>
2020-11-30 11:35:25 +0100

    fixed a small bug with navbar

--- public/script.js
+++ public/script.js
@@ -1,36 +1,39 @@
 console.log("sanity check");
 
-
 document.querySelector(".menu-icons").addEventListener("click", () => {
     document.querySelector(".navbar").classList.toggle("active");
 });
 
-window.onscroll = function() {
+window.onscroll = function () {
     growShrinkLogo();
 };
-  
-  
+
 function growShrinkLogo() {
     let logo = document.getElementsByClassName("navbar-logo")[0];
     let navbar = document.getElementsByClassName("navbar")[0];
     console.log(screen.width);
-    if (screen.width > 1100 && document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (
+        (screen.width > 1100 && document.body.scrollTop > 50) ||
+        document.documentElement.scrollTop > 50
+    ) {
         console.log("shrinking!");
         navbar.style.height = "6rem";
         navbar.style.backgroundColor = "black";
-        logo.style.width = '12rem';
+        logo.style.width = "12rem";
         logo.style.top = "0";
     } else {
-        navbar.style.height = "8rem";
-        navbar.style.background = "none";
-        if (screen.width > 1100) {
-            logo.style.top = "30px"; 
-            logo.style.width = '23rem';
+        if (navbar.classList.contains("prod")) {
+            navbar.style.height = "8rem";
+            navbar.style.background = "none";
         } else {
-            logo.style.top = "0px"; 
+            navbar.style.height = "8rem";
+        }
+        if (screen.width > 1100) {
+            logo.style.top = "30px";
+            logo.style.width = "23rem";
+        } else {
+            logo.style.top = "0px";
         }
         console.log("NOT shrinking!");
     }
-  }
-
-
+}
